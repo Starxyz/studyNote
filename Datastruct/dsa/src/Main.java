9343168d7c7bc99924a264a6bea86ddb2d96c14b
Starxy <a136009394@163.com>
2019-01-12 22:03:50 +0800

    binary search tree

--- Datastruct/dsa/src/Main.java
+++ Datastruct/dsa/src/Main.java
@@ -2,24 +2,13 @@ public class Main
 {
     public static void main(String[] args)
     {
-        LinkedList<Integer> list = new LinkedList<Integer>();
-
-        for (int i = 0; i < 5; i++)
+        BST<Integer> bst = new BST<>();
+        int[] nums = {5, 3, 6, 8, 4, 2};
+        for (int num : nums)
         {
-            list.addFirst(i);
-            System.out.println(list);
+            bst.add(num);
         }
 
-        list.add(2, 66);
-        System.out.println(list);
-
-        list.remove(2);
-        System.out.println(list);
-        list.removeLast();
-        System.out.println(list);
-
-        list.removeFirst();
-        System.out.println(list);
-
+        bst.levelOrder();
     }
 }
